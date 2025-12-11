f86deb099a9a0575a8392b0c6d525a1c8c16c412
subhakantadev <subhakantadev@gmail.com>
2025-12-11 18:49:27 +0530

    Api created section 2 end

---
+++ Persistence/Migrations/20251211111724_tablecloumechanged.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class tablecloumechanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Time",
+                table: "Activities",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "Catagory",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Activities",
+                newName: "Time");
+
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Catagory");
+        }
+    }
+}
